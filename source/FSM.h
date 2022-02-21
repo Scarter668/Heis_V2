2cbe79a93f45c4c7b12c77aa05ddd6b70f37ae23
Benjamin Elias Berg <benjaeb@stud.ntnu.no>
2022-02-21 18:12:22 +0100

    Defining enums and structs

---
+++ source/FSM.h
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * @brief Headerfile for finite state machine
+ */
+
+#pragma once
+#include <defines.h>
+#include <driver/elevio.h>
+
+
+typedef enum {
+    ElevatorDirectionDown = 0,
+    ElevatorDirectionUp,
+    ElevatorNoDirection
+} ElevatorDirection;
+
+typedef enum {
+    ElevatorStateInit = 0,
+    ElevatorStateIdle,
+    ElevatorStateDown,
+    ElevatorStateUp,
+    ElevatorStateEmergency
+} ElevatorState;
+
+typedef struct {
+    bool door;
+    bool obstruction;
+    bool emergency_btn;
+    ElevatorDirection direction;
+    double floor_level;
+} ElevatorVariables;
+
+void FSM_init();
+void FSM_update();
+
+
